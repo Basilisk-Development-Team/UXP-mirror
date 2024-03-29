25aa18f502bb20c44bcdec347186308ac8488413
win7-7 <win7-7@users.noreply.github.com>
2024-03-29 18:38:11 +0200

    1323242 -  Ignore spurious wakeups when waiting for background sweeping to finish
    1323242 - Ignore spurious wakeups when waiting for background thread to finish sweeping.

--- js/src/jsgc.cpp
+++ js/src/jsgc.cpp
@@ -3150,7 +3150,8 @@ GCHelperState::startBackgroundThread(State newState, const AutoLockGC& lock,
 void
 GCHelperState::waitForBackgroundThread(js::AutoLockGC& lock)
 {
-    done.wait(lock.guard());
+    while (isBackgroundSweeping())
+        done.wait(lock.guard());
 }
 
 void
