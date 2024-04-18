ccc0606ca93e8fe0c29081355f494271d8ef9dad
win7-7 <win7-7@users.noreply.github.com>
2024-04-18 22:46:34 +0300

    1364346 - Optimize Array.prototype.unshift
    1364346 part 1 - Optimize Array.prototype.unshift fast path and use it more.
    
    1364346 part 2 - Rename unshiftElements to moveShiftedElements, tweak heuristics.
    
    1364346 part 3 - Optimize Array.prototype.unshift by taking advantage of shifted elements.

--- js/src/vm/NativeObject-inl.h
+++ js/src/vm/NativeObject-inl.h
@@ -184,12 +184,20 @@ NativeObject::tryShiftDenseElements(uint32_t count)
         return false;
     }
 
+    shiftDenseElementsUnchecked(count);
+    return true;
+}
+
+inline void
+NativeObject::shiftDenseElementsUnchecked(uint32_t count)
+{
+    ObjectElements* header = getElementsHeader();
     MOZ_ASSERT(count > 0);
     MOZ_ASSERT(count < header->initializedLength);
     MOZ_ASSERT(count <= ObjectElements::MaxShiftedElements);
 
     if (MOZ_UNLIKELY(header->numShiftedElements() + count > ObjectElements::MaxShiftedElements)) {
-        unshiftElements();
+        moveShiftedElements();
         header = getElementsHeader();
     }
 
@@ -199,7 +207,6 @@ NativeObject::tryShiftDenseElements(uint32_t count)
     elements_ += count;
     ObjectElements* newHeader = getElementsHeader();
     memmove(newHeader, header, sizeof(ObjectElements));
-    return true;
 }
 
 inline void
