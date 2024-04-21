5facb815d9bfaecf8ed09ebce7d018dba25a152f
win7-7 <win7-7@users.noreply.github.com>
2024-04-22 01:53:46 +0300

    1354733 -  Avoid wrapping dead wrappers into CCWs
    1354733: Part 1 - Allow creating DeadObjectProxies directly.
    
    1354733: Part 2 - Never rewrap dead wrappers.

--- js/src/jsfriendapi.h
+++ js/src/jsfriendapi.h
@@ -111,7 +111,18 @@ JS_FRIEND_API(JS::Value) MaybeGetScriptPrivate(JSObject* object);
 
 }  // namespace js
 
-/*
+
+/**
+ * Creates a new dead wrapper object in the given scope. To be used when
+ * attempting to wrap objects from scopes which are already dead.
+ *
+ * If origObject is passed, it must be an proxy object, and will be
+ * used to determine the characteristics of the new dead wrapper.
+ */
+extern JS_FRIEND_API(JSObject*)
+JS_NewDeadWrapper(JSContext* cx, JSObject* origObject = nullptr);
+
+ /*
  * Used by the cycle collector to trace through a shape or object group and
  * all cycle-participating data it reaches, using bounded stack space.
  */
