e6067db46ed0e9a622a0c94cbd332c1ba72e8ae9
win7-7 <win7-7@users.noreply.github.com>
2024-04-29 01:31:34 +0300

    1367900 - Record the values and thresholds for GC triggers
    1367900 - Record the values and thresholds for GC triggers.

--- js/src/gc/Statistics.cpp
+++ js/src/gc/Statistics.cpp
@@ -751,6 +751,11 @@ Statistics::formatJsonSliceDescription(unsigned i, const SliceData& slice, JSONP
     json.property("initial_state", gc::StateName(slice.initialState));
     json.property("final_state", gc::StateName(slice.finalState));
     json.property("budget", budgetDescription);
+    json.property("major_gc_number", startingMajorGCNumber);
+    if (thresholdTriggered) {
+        json.floatProperty("trigger_amount", triggerAmount, 0);
+        json.floatProperty("trigger_threshold", triggerThreshold, 0);
+    }
     json.property("page_faults", int64_t(slice.endFaults - slice.startFaults));
     json.property("start_timestamp", slice.start - originTime, JSONPrinter::SECONDS);
     json.property("end_timestamp", slice.end - originTime, JSONPrinter::SECONDS);
@@ -798,6 +803,9 @@ Statistics::Statistics(JSRuntime* rt)
     fp(nullptr),
     nonincrementalReason_(gc::AbortReason::None),
     preBytes(0),
+    thresholdTriggered(false),
+    triggerAmount(0.0),
+    triggerThreshold(0.0),
     maxPauseInInterval(0),
     phaseNestingDepth(0),
     activeDagSlot(PHASE_DAG_NONE),
@@ -965,6 +973,7 @@ Statistics::endGC()
 
     // Clear the OOM flag.
     aborted = false;
+    thresholdTriggered = false;
 }
 
 void
