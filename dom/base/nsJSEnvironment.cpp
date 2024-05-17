1e0c11e56ecce4c557162b4834cb8f99593ae0a0
win7-7 <win7-7@users.noreply.github.com>
2024-05-17 15:44:41 +0300

    Revert "Attempting add CC delay to scheduler."
    This reverts commit 374cc99ce25037ba9321041c08ae5c2c8f90b7e8.
    
    Won't work. Due lack of definition later.

--- dom/base/nsJSEnvironment.cpp
+++ dom/base/nsJSEnvironment.cpp
@@ -141,9 +141,6 @@ static SATSState
 TriggerFullGC(uint32_t aCurrentID, void* aData);
 
 static SATSState
-CCDelay(uint32_t aCurrentID, void* aData);
-
-static SATSState
 ShrinkGCBuffers(uint32_t aCurrentID, void* aData);
 
 static SATSState
@@ -166,7 +163,6 @@ public:
     eFullGC,	
     eShrinkingGC,
     eForgetSkippable,	
-    eCCDelay,
     eCCSlice,
     eNone	
   };
@@ -179,7 +175,6 @@ static DependentSlowTask sMainThreadCollectorScheduling[]
   { CollectorSchedule::eVariableScheduledGC, 0,      TriggerGCOrGCSlice },
   { CollectorSchedule::eGCSlice,             100,    TriggerGCSlice },
   { CollectorSchedule::eFullGC,              60000,  TriggerFullGC },
-  { CollectorSchedule::eCCDelay,             20000,  CCDelay },
   { CollectorSchedule::eShrinkingGC,         300000, TriggerShrinkingGC },
   { CollectorSchedule::eForgetSkippable,     250,    TriggerForgetSkippable },
   { CollectorSchedule::eCCSlice,             32,     TriggerICCSlice },
