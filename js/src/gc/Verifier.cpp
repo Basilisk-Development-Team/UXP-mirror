85f6eff03e4c0bb74abd52e02ee599ee928aead0
win7-7 <win7-7@users.noreply.github.com>
2026-01-11 03:50:40 +0200

    903519 - Remove verifier assumption that only objects are in the nursery
    903519 - Remove verifier assumption that only objects are in the nursery. This is only relevant after 55 verifier.

--- js/src/gc/Verifier.cpp
+++ js/src/gc/Verifier.cpp
@@ -529,8 +529,15 @@ HeapCheckTracerBase::onChild(const JS::GCCellPtr& thing)
         return;
 
     // Don't trace into GC in zones being used by helper threads.
-    Zone* zone = thing.is<JSObject>() ? thing.as<JSObject>().zone() : cell->asTenured().zone();
-    if (zone->group() && zone->group()->usedByHelperThread())
+    Zone* zone;
+    if (thing.is<JSObject>())
+        zone = thing.as<JSObject>().zone();
+    else if (thing.is<JSString>())
+        zone = thing.as<JSString>().zone();
+    else
+        zone = cell->asTenured().zone();
+
+    if (zone->group() && zone->group()->usedByHelperThread)
         return;
 
     WorkItem item(thing, contextName(), parentIndex);
