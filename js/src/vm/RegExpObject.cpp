2bc5bb2397debec735599d7f29e523b8f5e69771
win7-7 <win7-7@users.noreply.github.com>
2026-01-18 09:29:32 +0200

    Fix remaining errors from mixing two different approaches.
    Fix remaining errors from mixing two different approaches.

--- js/src/vm/RegExpObject.cpp
+++ js/src/vm/RegExpObject.cpp
@@ -958,16 +958,17 @@ RegExpShared::RegExpShared(JSAtom* source, RegExpFlag flags)
 {}
 
 struct RegExpShared::NamedCaptureData {
-	bool mapInited;
     // per-compartment template cache (optional; you can also skip templates entirely)
     using GroupsTemplateMap =
         GCHashMap<JSCompartment*, ReadBarriered<PlainObject*>,
                   DefaultHasher<JSCompartment*>, SystemAllocPolicy>;
     GroupsTemplateMap groupsTemplateMap;
-    bool groupsTemplateMapInited = false;
+    bool groupsTemplateMapInited;
 
     Vector<JSAtom*, 0, SystemAllocPolicy> names;
     Vector<uint32_t, 0, SystemAllocPolicy> indices;
+
+    NamedCaptureData() : groupsTemplateMapInited(false) {}
 };
 
 void
@@ -977,14 +978,16 @@ RegExpShared::traceChildren(JSTracer* trc)
     if (IsMarkingTrace(trc) && trc->runtime()->gc.isShrinkingGC())
         discardJitCode();
 
-    if (namedCaptureData_ && namedCaptureData_->mapInited) {
+    if (namedCaptureData_ && namedCaptureData_->groupsTemplateMapInited) {
 	  for (auto iter = namedCaptureData_->groupsTemplateMap.all(); !iter.empty(); iter.popFront()) {
         TraceNullableEdge(trc, &iter.front().value(), "RegExpShared groupsTemplate per-compartment");
       }
     }
 
-    for (auto& atom : namedCaptureNames_) {
-    TraceEdge(trc, &atom, "RegExpShared namedCaptureName");
+    if (namedCaptureData_) {
+      for (JSAtom*& atom : namedCaptureData_->names) {
+        TraceEdge(trc, &atom, "RegExpShared namedCaptureName");
+      }
     }
 
     TraceNullableEdge(trc, &source, "RegExpShared source");
@@ -1005,6 +1008,11 @@ RegExpShared::discardJitCode()
 void
 RegExpShared::finalize(FreeOp* fop)
 {
+	if (namedCaptureData_) {
+        js_delete(namedCaptureData_);
+        namedCaptureData_ = nullptr;
+    }
+
     for (auto& comp : compilationArray)
         js_free(comp.byteCode);
     tables.~JitCodeTables();
@@ -1394,10 +1402,10 @@ RegExpShared::getOrCreateGroupsTemplate(JSContext* cx)
 
     NamedCaptureData* data = namedCaptureData_;
 
-    if (!data->mapInited) {
+    if (!data->groupsTemplateMapInited) {
         if (!data->groupsTemplateMap.init(4))
             return nullptr;
-        data->mapInited = true;
+        data->groupsTemplateMapInited = true;
     }
 
     JSCompartment* comp = cx->compartment();
