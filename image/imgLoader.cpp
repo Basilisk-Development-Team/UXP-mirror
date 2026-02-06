c6bc9d5a8cd81d8713222b8eca2e41a500a9fa57
mittorn <mittorn@disroot.org>
2026-02-06 23:14:54 +0000

    Issue #2932 - Ensure that imgRequestProxy::CancelAndForgetObserver removes itself from the cache validator
    https://hg-edge.mozilla.org/releases/mozilla-beta/rev/af82d0c33427

--- image/imgLoader.cpp
+++ image/imgLoader.cpp
@@ -2730,6 +2730,12 @@ imgCacheValidator::AddProxy(imgRequestProxy* aProxy)
   mProxies.AppendObject(aProxy);
 }
 
+void
+imgCacheValidator::RemoveProxy(imgRequestProxy* aProxy)
+{
+  mProxies.RemoveObject(aProxy);
+}
+
 /** nsIRequestObserver methods **/
 
 NS_IMETHODIMP
--- image/imgLoader.h
+++ image/imgLoader.h
@@ -541,6 +541,7 @@ public:
                     bool forcePrincipalCheckForCacheEntry);
 
   void AddProxy(imgRequestProxy* aProxy);
+  void RemoveProxy(imgRequestProxy* aProxy);
 
   NS_DECL_ISUPPORTS
   NS_DECL_NSITHREADRETARGETABLESTREAMLISTENER
