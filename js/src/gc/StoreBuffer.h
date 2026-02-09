74aabee88b4487aa2538f3f78bafa5615988f865
Moonchild <moonchild@palemoon.org>
2026-02-09 16:06:44 +0100

    Name the Hasher struct definition to align with c++17 standard

--- js/src/gc/StoreBuffer.h
+++ js/src/gc/StoreBuffer.h
@@ -340,7 +340,7 @@ class StoreBuffer
 
         explicit operator bool() const { return objectAndKind_ != 0; }
 
-        typedef struct {
+        typedef struct HasherType {
             typedef SlotsEdge Lookup;
             static HashNumber hash(const Lookup& l) {
                 return mozilla::HashGeneric(l.objectAndKind_, l.start_, l.count_);
