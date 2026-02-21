84da3d4817ec60151b567ae64e6e1d04763b9f83
Moonchild <moonchild@palemoon.org>
2026-02-21 19:23:42 +0100

    Issue #190 - Fix incorrectly applied patch.
    For some reason the rebase removed the endif in the wrong place...

--- config/rules.mk
+++ config/rules.mk
@@ -349,8 +349,8 @@ ifdef IS_COMPONENT
 EXTRA_DSO_LDOPTS	+= -bundle
 else
 _LOADER_PATH := @executable_path
-endif
 EXTRA_DSO_LDOPTS	+= -dynamiclib -install_name $(_LOADER_PATH)/$(SHARED_LIBRARY) -compatibility_version 1 -current_version 1 -single_module
+endif
 endif
 endif
 
