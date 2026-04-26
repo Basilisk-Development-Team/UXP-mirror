6dcb20949a8935672bb174046178bf3f449abd72
Moonchild <moonchild@palemoon.org>
2026-04-26 17:06:16 +0200

    [NSS] Restore dropped 'S' :P

--- security/nss/lib/softoken/softkver.h
+++ security/nss/lib/softoken/softkver.h
@@ -17,7 +17,7 @@
  * The format of the version string should be
  *     "<major version>.<minor version>[.<patch level>[.<build number>]][ <ECC>][ <Beta>]"
  */
-#define SOFTOKEN_VERSION "3.90.10.0 (UXP)" OFTOKEN_ECC_STRING
+#define SOFTOKEN_VERSION "3.90.10.0 (UXP)" SOFTOKEN_ECC_STRING
 #define SOFTOKEN_VMAJOR 3
 #define SOFTOKEN_VMINOR 90
 #define SOFTOKEN_VPATCH 10
