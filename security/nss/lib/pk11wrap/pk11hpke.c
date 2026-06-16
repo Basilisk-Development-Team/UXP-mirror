91b3b6fdc86dac679e9c2b8f42cf2283f9805fbb
John Schanck <jschanck@mozilla.com>
2026-06-16 09:27:28 +0200

    Bug 2029803 - add defensive info->len check in PK11_HPKE_SetupS and PK11_HPKE_SetupR.

--- security/nss/lib/pk11wrap/pk11hpke.c
+++ security/nss/lib/pk11wrap/pk11hpke.c
@@ -1079,7 +1079,8 @@ PK11_HPKE_SetupR(HpkeContext *cx, const SECKEYPublicKey *pkR, SECKEYPrivateKey *
     SECStatus rv;
     SECItem empty = { siBuffer, NULL, 0 };
 
-    CHECK_FAIL_ERR((!cx || !skR || !info || !enc || !enc->data || !enc->len),
+    CHECK_FAIL_ERR((!cx || !skR || !info || !enc || !enc->data || !enc->len ||
+                    info->len > 0xFFFF),
                    SEC_ERROR_INVALID_ARGS);
     /* Already setup */
     CHECK_FAIL_ERR((cx->aeadContext), SEC_ERROR_INVALID_STATE);
@@ -1113,7 +1114,8 @@ PK11_HPKE_SetupS(HpkeContext *cx, const SECKEYPublicKey *pkE, SECKEYPrivateKey *
     SECItem empty = { siBuffer, NULL, 0 };
     SECKEYPublicKey *tmpPkE = NULL;
     SECKEYPrivateKey *tmpSkE = NULL;
-    CHECK_FAIL_ERR((!cx || !pkR || !info || (!!skE != !!pkE)), SEC_ERROR_INVALID_ARGS);
+    CHECK_FAIL_ERR((!cx || !pkR || !info || (!!skE != !!pkE) || info->len > 0xFFFF),
+                    SEC_ERROR_INVALID_ARGS);
     /* Already setup */
     CHECK_FAIL_ERR((cx->aeadContext), SEC_ERROR_INVALID_STATE);
 
